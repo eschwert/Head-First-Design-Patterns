fc9573e39691aa8e241344ce29698898b5e502d4
eschwert <eschwert@arcor.de>
2017-06-11 20:30:36 +0200

    Merge remote-tracking branch 'origin/esc' into esc
    Conflicts:
    	src/headfirst/designpatterns/adapter/ducks/Duck.java

--- src/headfirst/designpatterns/adapter/ducks/Duck.java
+++ src/headfirst/designpatterns/adapter/ducks/Duck.java
@@ -4,6 +4,12 @@ public interface Duck {
 	public void quack();
 	public void fly();
 
+	public void walk2();
+
+	public void walk3();
+
 	public void walk();
 	public void jump();
+
+	public void walk3();
 }
